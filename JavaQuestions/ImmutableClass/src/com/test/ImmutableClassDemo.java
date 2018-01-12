8e8ae19e4547a80ffc32a1e0a860296a9374b0d8
unknown <manohar427@gmail.com>
2018-01-12 22:48:14 +0530

    Java imp questions

--- JavaQuestions/ImmutableClass/src/com/test/ImmutableClassDemo.java
+++ JavaQuestions/ImmutableClass/src/com/test/ImmutableClassDemo.java
@@ -1,7 +1,14 @@
 package com.test;
 
 public class ImmutableClassDemo {
-   public static void main(String[] args) {
-	
-}
+	public static void main(String[] args) {
+		Age age = new Age();
+		age.setDay(1);
+		age.setMonth(1);
+		age.setYear(1992);
+		ImmutableStudent student = new ImmutableStudent(1, "Alex", age);
+		System.out.println("Alex age year before modification = " + student.getAge().getYear());
+		student.getAge().setYear(1993);
+		System.out.println("Alex age year after modification = " + student.getAge().getYear());
+	}
 }
